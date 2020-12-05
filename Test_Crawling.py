3ed2880034ebc01b4f64e46869b414cfdb6c85e1
JJinTae <46085058+JJinTae@users.noreply.github.com>
2020-12-05 22:52:51 +0900

    test_crawling

---
+++ Test_Crawling.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.inflearn.com/courses/it-programming/mobile-app"
+
+response = requests.get(url=URL)
+soup = BeautifulSoup(response.text, "html.parser")
+
+get_all_course = soup.find_all("div", {"class":"course_title"})
+
+print(get_all_course)
+
+def parsing_course(elements):
+    print(elements.find(""))
+    print("-------")
+
+for i in get_all_course:
+    # parsing_course(i)
+    print(i)
+
+
