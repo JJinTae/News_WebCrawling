be7696232d1a54099d02f278380c815ea04d56cf
JJinTae <46085058+JJinTae@users.noreply.github.com>
2020-12-07 02:03:51 +0900

    만료된 청원 번호 가져오기 구현
    인성/성평등 탭에서만 가능

--- Test_Crawling.py
+++ Test_Crawling.py
@@ -1,35 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import time
-
-
-URL = "https://www1.president.go.kr/petitions/?c=49&only=2&page=1&order=1"
-
-response = requests.get(url=URL)
-soup = BeautifulSoup(response.text, "html.parser")
-
-get_all_course = soup.find_all("div", {"class":"bl_no"})
-
-print(get_all_course)
-
-
-
-def parsing_course(elements):
-    print(elements.text)
-    print("-------")
-
-
-for i in get_all_course:
-    parsing_course(i)
-    # print(i)
-
 
 
 
 def main():
     while True:
+        run()
         try:
-            run()
+            pass
             break
         except:
             print('wait for server 5 seconds...')
@@ -45,14 +25,16 @@ def run():
     print(get_latest_petition_num())
 
 
+
+# 인권 / 성평등 만료청원 가장 최근에 만료된 청원 번호 가져오기
 def get_latest_petition_num():
     URL = "https://www1.president.go.kr/petitions/?c=49&only=2&page=1&order=1"
-    html = requests.get(url=URL)
-    soup = BeautifulSoup(html.text, "html.parser")
 
-    latest_petition_num = soup.find_all("div", {"class": "bl_no"})
+    driver = webdriver.Chrome('chromedriver_win32/chromedriver.exe')
+    driver.get(URL)
+    time.sleep(2)
 
-
+    latest_petition_num = int(driver.find_element_by_xpath("/html/body/div[2]/div[2]/section[2]/div[2]/div/div/div[2]/div[2]/div[4]/div/div[2]/div[2]/ul/li[1]/div/div[1]").text)
 
     return latest_petition_num
 
