f59e77949ab671b46b2f58301c46b8a98753e582
JJinTae <46085058+JJinTae@users.noreply.github.com>
2020-12-09 05:17:07 +0900

    konlpy를 사용한 명사 분석
    konlpy를 사용한 명사 분석을 통한 키워드 검출이 가능하게 하였다.
    
    + 추가적으로 stopwords를 추가하여 불용성단어를 배제하였다.

--- Data_analysis.py
+++ Data_analysis.py
@@ -7,9 +7,42 @@ from soynlp.tokenizer import RegexTokenizer
 from soynlp.utils import DoublespaceLineCorpus
 from soynlp.noun import LRNounExtractor_v2
 
-DATA_DIR = 'Data_CSV'
-CSV_PETITION = os.path.join(DATA_DIR, 'petition_result_v2.csv')
+from konlpy.tag import Okt
+from collections import Counter
 
+DATA_DIR = 'Data_CSV'
+CSV_PETITION = os.path.join(DATA_DIR, 'petition_result_v2_content_M10.csv')
+
+STOPWORDS_TXT = open('stopwords.txt', 'r', encoding='utf-8')
+stopwords = STOPWORDS_TXT.readlines()
+for i in range(len(stopwords)):
+    stopwords[i] = stopwords[i].strip()
+
+
+# knolpy
+f = open(CSV_PETITION, "r", encoding='utf-8')
+lines = f.read()
+
+nlpy = Okt()
+nouns = nlpy.nouns(lines)
+
+count = Counter(nouns)
+
+tag_count = []
+tags = []
+
+for n, c in count.most_common(50):
+    dics = {'tag': n, 'count':c}
+    if len(dics['tag']) >=2 and len(tags) <= 19:
+        if n not in stopwords:
+            tag_count.append(dics)
+            tags.append(dics['tag'])
+
+for tag in tag_count:
+    print(" {:<14}".format(tag['tag']), end='\t')
+    print("{}".format(tag['count']))
+
+"""
 def preprocessing(text):
     text = re.sub('\\\\n', ' ', text)
     return text
@@ -37,5 +70,4 @@ noun_extractor = LRNounExtractor_v2(verbose=True)
 nouns = noun_extractor.train_extract(sents)
 
 print(sorted(nouns.items(), key=lambda t : t[1], reverse=True))
-
-
+"""
